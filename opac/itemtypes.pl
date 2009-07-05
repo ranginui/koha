68a3231cbdc54461a5d5bb5a38e6f8996b62dc50
Chris Cormack <chrisc@catalyst.net.nz>
2009-07-05 23:26:51 +0000

    Fixing to use circ codes not itemtypes for the opac homepage

--- opac/itemtypes.pl
+++ opac/itemtypes.pl
@@ -1,30 +1,56 @@
 #!/usr/bin/perl
 
+# Written by bob@katipo.co.nz
+# Modified by chrisc@catalyst.net.nz 6/7/2009
+
 use strict;
-require Exporter;
+use warnings;
 use CGI;
-use C4::Auth;    # get_template_and_user
-use C4::Output;
 use C4::Koha;
+use C4::Context;
 
 my $input = new CGI;
-# get currently used itemtypes
-my $itemtypes = &GetItemTypes();
 
+# get currently used itemtypes
+my $advanced_search_types = C4::Context->preference("AdvancedSearchTypes");
+my $itemtypes;
+my $cnt=1;
+if ($advanced_search_types) {
+    my $advsearchtypes = GetAuthorisedValues($advanced_search_types);
+    my @itemtypesloop;
+    for my $thisitemtype (@$advsearchtypes) {
+        my %row = (
+            number      => $cnt++,
+            ccl         => $advanced_search_types,
+            code        => $thisitemtype->{authorised_value},
+   #         selected    => $selected,
+            description => $thisitemtype->{'lib'},
+            count5      => $cnt % 4,
+            imageurl =>
+              getitemtypeimagelocation( 'opac', $thisitemtype->{'imageurl'} ),
+        );
+        push @itemtypesloop, \%row;
+    }
+    $itemtypes = \@itemtypesloop;
+}
 print $input->header;
-if($itemtypes){
-    print '<form name="searchform" method="get" action="/cgi-bin/koha/opac-search.pl">';
+if ($itemtypes) {
+    print
+'<form name="searchform" method="get" action="/cgi-bin/koha/opac-search.pl">';
     print '<input type="hidden" name="idx" value="kw">';
     print '<input type="hidden" name="sort_by" value="acqdate_dsc">';
     print '<input type="hidden" name="do" value="OK">';
-    if($input->param('just_arrived')){
-	print '<input type="hidden" name="limit" value="available">';	
+    if ( $input->param('just_arrived') ) {
+        print '<input type="hidden" name="limit" value="available">';
     }
     print '<select name="limit" onChange="this.form.submit()">';
-    foreach my $thisitemtype (sort keys %$itemtypes) {
-	print '<option value="mc-itype:'.$thisitemtype.'">'.$itemtypes->{$thisitemtype}->{'description'}.'</option>';
+    foreach my $thisitemtype ( sort {$a->{description} cmp $b->{description}} @$itemtypes ) {
+        print '<option value="mc-ccode:'
+          . $thisitemtype->{code} . '">'
+          . $thisitemtype->{'description'}
+          . '</option>';
     }
     print '</select>';
-    print '</form>';    
+    print '</form>';
 }
 1;
