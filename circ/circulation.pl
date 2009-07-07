da97c5eb79ddfacd663b29cf578ca7dbb390c0e7
Jean-André Santoni <jeanandre.santoni@biblibre.com>
2009-07-07 12:19:22 +0200

    [3.0.x](bug #3349) Display full borrower address
    This patch fixes the truncated address in /circ/circulation.pl

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -644,6 +644,10 @@ if ( C4::Context->preference("memberofinstitution") ) {
     );
 }
 
+# Computes full borrower address
+my (undef, $roadttype_hashref) = &GetRoadTypes();
+my $address = $borrower->{'streetnumber'}.' '.$roadttype_hashref->{$borrower->{'streettype'}}.' '.$borrower->{'address'};
+
 $template->param(
     issued_itemtypes_count_loop => \@issued_itemtypes_count_loop,
     findborrower                => $findborrower,
@@ -659,7 +663,7 @@ $template->param(
     expiry            => format_date($borrower->{'dateexpiry'}),
     categorycode      => $borrower->{'categorycode'},
     categoryname      => $borrower->{description},
-    address           => $borrower->{'address'},
+    address           => $address,
     address2          => $borrower->{'address2'},
     email             => $borrower->{'email'},
     emailpro          => $borrower->{'emailpro'},
