540304beaed4f89d81396050caa18e6f256aeb5d
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2009-07-24 14:25:40 +0200

    UpdateDatabase first quick fix

--- kohaversion.pl
+++ kohaversion.pl
@@ -10,7 +10,7 @@
 use strict;
 
 sub kohaversion {
-    our $VERSION = '3.00.03.100';
+    our $VERSION = '3.00.04.037';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
