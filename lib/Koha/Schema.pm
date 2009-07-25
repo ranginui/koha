b68bfca6c905aef52096c70c7c0a811162b72199
Chris Cormack <chris@bigballofwax.co.nz>
2009-07-25 20:09:56 +1200

    Adding License info

--- lib/Koha/Schema.pm
+++ lib/Koha/Schema.pm
@@ -1,5 +1,21 @@
 package Koha::Schema;
 
+# Copyright 2009 Chris Cormack <chrisc@catalyst.net.nz>                                                                                                                   
+#                                                                                                                                                                         
+# This file is part of Koha.                                                                                                                                              
+#                                                                                                                                                                         
+# Koha is free software; you can redistribute it and/or modify it under the                                                                                               
+# terms of the GNU General Public License as published by the Free Software                                                                                               
+# Foundation; either version 3 of the License, or (at your option) any later                                                                                              
+# version.                                                                                                                                                                
+#                                                                                                                                                                         
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY                                                                                                 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR                                                                                           
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.                                                                                             
+#                                                                                                                                                                         
+# You should have received a copy of the GNU General Public License along with                                                                                            
+# Koha; If not, see <http://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
 
@@ -7,10 +23,4 @@ use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_classes;
 
-
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-07-25 19:16:56
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:QSnc/8bSI8z9W9CwJRo59g
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 1;
