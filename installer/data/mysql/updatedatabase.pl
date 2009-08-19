491c51a8513ae7d3604efe1cd3f6dda8ce3b1f9c
Chris Cormack <chrisc@catalyst.net.nz>
2009-08-19 08:16:09 +0000

    Bug 3539, Adding a systempreference to hide borrowers name on the catalogue detail page, and holds page
    Squashed commit of the following:
    
    commit e9dd6e1dee03fe3503f7995b57510f9edfe6b333
    Author: Chris Cormack <chrisc@catalyst.net.nz>
    Date:   Wed Aug 19 08:14:53 2009 +0000
    
        Version number
    
    commit df3ee5473e79b760a91c62b96d7ce19229e0fd27
    Author: Chris Cormack <chrisc@catalyst.net.nz>
    Date:   Wed Aug 19 08:08:11 2009 +0000
    
        French systempref
    
    commit c4b036e4e43947d7d1fb9f2e18e460788cb4e1ec
    Author: Chris Cormack <chrisc@catalyst.net.nz>
    Date:   Wed Aug 19 00:25:33 2009 +0000
    
        Code to update the database, and updating the sysprefs.sql
    
    commit f74b942c24d5e9c33a4ec0e5ac706a0e347eb1a7
    Author: Chris Cormack <chrisc@catalyst.net.nz>
    Date:   Tue Aug 18 02:07:22 2009 +0000
    
        Now hides the name on the detail screen too
    
    commit 53c80dbef88e360a4bbb78ecff3a6ca6ec401398
    Author: Chris Cormack <chrisc@catalyst.net.nz>
    Date:   Tue Aug 18 01:35:19 2009 +0000
    
        Allowing the librarians to choose to hide the borrowers name on holds

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -2503,6 +2503,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     print "Upgrade to $DBversion done (added AWSPrivateKey syspref - note that if you use enhanced content from Amazon, this should be set right away.)";
 }
 
+$DBversion = '3.01.00.045';
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    $dbh->do("INSERT INTO systempreferences (variable,value,options,explanation,type)VALUES('HidePatronName', '0', '', 'If this is switched on, patrons cardnumbers will be shown instead of their name on the holds and catalogue screens', 'YesNo')");
+    SetVersion ($DBversion);
+    print "Upgrade to $DBversion done (added a preference to hide the patrons name in the staff catalog)";
+}
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
