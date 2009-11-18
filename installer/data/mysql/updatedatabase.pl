e2b28e18f0bd2e3b192911d2863067bf2346f0a4
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2009-11-18 16:25:39 +0100

    Updatedatabase String for reports update

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3635,7 +3635,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
 	);
 	});
 
-    print "Upgrade to $DBversion done (isbd updated)\n";
+    print "Upgrade to $DBversion done (adding one table : pending_offline_operations)\n";
     SetVersion ($DBversion);
 }
 
@@ -3693,7 +3693,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
 	ALTER TABLE items ADD statisticvalue VARCHAR(80);
 	});
 	
-    print "Upgrade to $DBversion done (isbd updated)\n";
+    print "Upgrade to $DBversion done (adding statisticvalue field to items)\n";
     SetVersion ($DBversion);
 }
 
