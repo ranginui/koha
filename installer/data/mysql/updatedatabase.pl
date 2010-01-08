37088e5734c6fa13401a4474f6863904b7e146d2
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2010-01-08 13:37:47 +0100

    renewalperiod problem

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3758,7 +3758,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
 
 $DBversion = '3.02.00.011';
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
-    $dbh->do('ALTER TABLE issuingrules ADD COLUMN `renewalperiod` int(11) NOT NULL default "0" AFTER `renewalsallowed`;');
+    $dbh->do('ALTER TABLE issuingrules ADD COLUMN `renewalperiod` SMALLINT(6) NULL default NULL AFTER `renewalsallowed`;');
     print "Upgrade done (Add renewalperiod)\n";
     SetVersion ($DBversion);
 }
