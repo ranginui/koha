62a98951d2ab9f685cc031c786a2a89fae4ff894
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-02-01 15:18:39 +0100

    (MT2690) fix ordering in member list
    This fix the order of member list

--- members/member.pl
+++ members/member.pl
@@ -75,7 +75,7 @@ if (C4::Context->preference("AddPatronLists")=~/code/){
 my $member=$input->param('member');
 my $orderbyparams=$input->param('orderby');
 my @orderby;
-if ($orderbyparams){
+if ($orderbyparams and not $quicksearch){
 	my @orderbyelt=split(/,/,$orderbyparams);
 	push @orderby, {$orderbyelt[0]=>$orderbyelt[1]||0};
 }
@@ -107,6 +107,7 @@ if($count == 1){
 my @resultsdata;
 my $to=($count>$to?$to:$count);
 my $index=$from;
+
 foreach my $borrower(@$results[$from..$to-1]){
   #find out stats
   my ($od,$issue,$fines)=GetMemberIssuesAndFines($$borrower{'borrowernumber'});
