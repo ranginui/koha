cb2bdb985fd138c122b9a6f4690c08d0ed12b536
Matthias Meusburger <matthias.meusburger@biblibre.com>
2010-03-30 17:26:56 +0200

    MT3132: Change gonenoaddress message in opac-user.pl according to the OPACPatronDetails syspref

--- opac/opac-user.pl
+++ opac/opac-user.pl
@@ -55,6 +55,7 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
 );
 
 my $OPACDisplayRequestPriority = (C4::Context->preference("OPACDisplayRequestPriority")) ? 1 : 0;
+my $OPACPatronDetails          = (C4::Context->preference("OPACPatronDetails")) ? 1 : 0;
 my $patronupdate = $query->param('patronupdate');
 
 # get borrower information ....
@@ -210,6 +211,7 @@ foreach my $res (@reserves) {
 $template->param( RESERVES       => \@reserves );
 $template->param( reserves_count => $#reserves+1 );
 $template->param( showpriority=>1 ) if $OPACDisplayRequestPriority;
+$template->param( OPACPatronDetails => 1 ) if $OPACPatronDetails;
 
 my @waiting;
 my $wcount = 0;
