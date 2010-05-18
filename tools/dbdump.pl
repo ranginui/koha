c00fe2de693bd2dc3267fef6938ab7aea695bc0c
Lars Wirzenius <lars@catalyst.net.nz>
2010-05-18 13:40:29 +1200

    Provide a download link to the database dump on the tools page.

---
+++ tools/dbdump.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Copyright 2010 Catalyst IT Ltd.
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+use strict;
+use warnings;
+
+use CGI;
+use C4::Auth;
+use C4::Output;
+
+my $query = new CGI;
+my ( $user, $cookie, $sessionID, $flags ) = checkauth(
+        $query,
+        0,
+        { tools => '*' },
+        "intranet"
+   );
+if ($flags->{superlibrarian}) {
+    my $kohaname = $ENV{"KOHA_CONF"};
+    $kohaname =~ s:^/etc/koha/sites/([^/]*)/koha-conf.xml$:$1:;
+    my @basenames = glob("/var/spool/koha/$kohaname/$kohaname-*.sql.gz");
+    my $filename = $basenames[-1];
+    open(DUMP, '<', $filename) or die("Can't read $filename");
+    binmode DUMP;
+
+    my $basename = $filename;
+    $basename =~ s:.*/::;
+    print $query->header(
+        -type => 'application/x-gzip',
+        -attachment => "$basename"
+    );
+    binmode STDOUT;
+
+    while (read(DUMP, my $data, 64 * 1024)) {
+        print $data;
+    }
+} else {
+    my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+        {
+            template_name   => "tools/dbdump-not-superlibrarian.tmpl",
+            query           => $query,
+            type            => "intranet",
+            authnotrequired => 0,
+            flagsrequired   => { tools => '*' },
+            debug           => 1,
+        }
+    );
+    output_html_with_http_headers $query, $cookie, $template->output;
+}
