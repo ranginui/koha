97b9822fc5ed137f65d48950af51bc769a474e68
Matthias Meusburger <matthias.meusburger@biblibre.com>
2010-05-20 16:37:19 +0200

    MT3597 : Fix patron attribute adding
    Adds a forgotten updatedatabase from "Member Improvements" patch

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4385,10 +4385,9 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do(q{
       ALTER TABLE borrower_attribute_types ADD `display_checkout` TINYINT(1) NOT NULL DEFAULT '0';
 });
-    print "Upgrade to $DBversion done. — Add display_checkout in borrowers attributes\n";
+    print "Upgrade to $DBversion done. — Add display_checkout in borrower_attribute_types table\n";
     SetVersion ($DBversion);
 }
-
 
 =item DropAllForeignKeys($table)
 
