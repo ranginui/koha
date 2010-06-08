bfbe1c80544e7763e7207f0e63a6600eaeef5b09
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2010-06-08 10:20:27 +0200

    MT3667 followup MT3811 : unexpected issues not stored
    As a consequence of previous MT3667 patch, the subscriptionid list was processed.
    And unfortunately, since array order is the only relevant information from HTML processing,
    it caused subscriptionid to be null for a double edition.
    This patch only uses uniq for serials-collection and when passing information to serials-collection page.

--- serials/serials-collection.pl
+++ serials/serials-collection.pl
@@ -27,6 +27,7 @@ use C4::Serials;
 use C4::Letters;
 use C4::Output;
 use C4::Context;
+use List::MoreUtils qw/uniq/;
 
 my $query = new CGI;
 my $op    = $query->param('op') || q{};
@@ -45,6 +46,7 @@ my ( $template, $loggedinuser, $cookie );
 my $biblionumber   = $query->param('biblionumber');
 my @subscriptionid = $query->param('subscriptionid');
 
+@subscriptionid= uniq @subscriptionid;
 my $subscriptiondescs;
 my $subscriptions;
 
