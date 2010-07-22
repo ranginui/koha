d93781b495ece6bfe2ae0caa0ad19e7d26e11596
Matthias Meusburger <matthias.meusburger@biblibre.com>
2010-07-22 11:56:46 +0200

    MT4029: Adds dynamic search for collection titles in 225$a plugin

---
+++ cataloguing/ysearch.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+# This software is placed under the gnu General Public License, v2 (http://www.gnu.org/licenses/gpl.html)
+
+# Copyright 2007 Tamil s.a.r.l.
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=head1 ysearch.pl
+
+
+=cut
+
+use strict;
+
+#use warnings; FIXME - Bug 2505
+use CGI;
+use C4::Context;
+use C4::Auth qw/check_cookie_auth/;
+
+my $input = new CGI;
+my $query = $input->param('query');
+
+binmode STDOUT, ":utf8";
+print $input->header( -type => 'text/plain', -charset => 'UTF-8' );
+
+my ( $auth_status, $sessionID ) = check_cookie_auth( $input->cookie('CGISESSID'), { cataloguing => '*' } );
+if ( $auth_status ne "ok" ) {
+    exit 0;
+}
+
+my $dbh = C4::Context->dbh;
+my $sql = qq(SELECT distinct collectiontitle 
+             FROM biblioitems 
+             WHERE collectiontitle LIKE ?);
+$sql .= qq( ORDER BY collectiontitle);
+my $sth = $dbh->prepare($sql);
+$sth->execute("%$query%");
+
+while ( my $rec = $sth->fetchrow_hashref ) {
+    print $rec->{collectiontitle} . "\n";
+}
