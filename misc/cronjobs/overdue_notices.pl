dcd10601098957bfbd5e9217afb6f0b72dbb5268
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-08-06 10:19:40 +0200

    (bug #5098) csv headers in overdues
    this add the csv column headers in mail sent to librarian.

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -600,6 +600,9 @@ END_SQL
             local $, = "\f";    # pagebreak
             print @output_chunks;
         }
+        my $content = join(";", qw(title name surname address1 address2 zipcode city email itemcount itemsinfo due_date issue_date)) . "\n";
+        $content .= join( "\n", @output_chunks );
+
         my $attachment = {
             filename => defined $csvfilename ? 'attachment.csv' : 'attachment.txt',
             type => 'text/plain',
