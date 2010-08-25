3a35e61d37f1f5eeecc362f3b0b8ef05a56d6e24
Robin Sheat <robin@catalyst.net.nz>
2010-08-25 14:15:43 +1200

    Set the templates so that the othername field is used if supplied.

--- C4/Members.pm
+++ C4/Members.pm
@@ -367,6 +367,11 @@ sub GetMemberDetails {
     $sth->execute( $borrower->{'categorycode'} );
     my $enrolment = $sth->fetchrow;
     $borrower->{'enrolmentperiod'} = $enrolment;
+    # For the purposes of making templates easier, we'll define a
+    # 'showname' which is the alternate form the user's first name if 
+    # 'other name' is defined.
+    $borrower->{'showname'} = $borrower->{'othernames'} || $borrower->{'firstname'};
+
     return ($borrower);    #, $flags, $accessflagshash);
 }
 
