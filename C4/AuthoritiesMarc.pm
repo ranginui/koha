4604e0f55a9e2aa66e4ba0080941805fa0ca8b72
Jean-André Santoni <jeanandre.santoni@biblibre.com>
2010-09-08 17:48:32 +0200

    Authorities Marc : adding support for itemtypes
    itemtypes are used in order to search for an authority type

--- C4/AuthoritiesMarc.pm
+++ C4/AuthoritiesMarc.pm
@@ -55,7 +55,6 @@ BEGIN {
 
       &CountUsage
       &CountUsageChildren
-      &SearchAuthorities
 
       &BuildSummary
       &BuildUnimarcHierarchies
@@ -694,8 +693,6 @@ sub FindDuplicateAuthority {
     my ($auth_tag_to_report) = $sth->fetchrow;
     $sth->finish;
 
-    #     warn "record :".$record->as_formatted."  auth_tag_to_report :$auth_tag_to_report";
-    # build a request for SearchAuthorities
     my $query        = 'at=' . $authtypecode . ' ';
     my $filtervalues = qr([\001-\040\!\'\"\`\#\$\%\&\*\+,\-\./:;<=>\?\@\(\)\{\[\]\}_\|\~]);
     if ( $record->field($auth_tag_to_report) ) {
