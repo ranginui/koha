d8779417ec9a7e9e1a5e05caa9ae24d9d73f7ba2
Jean-André Santoni <jeanandre.santoni@biblibre.com>
2010-10-13 10:37:00 +0200

    Fixes a bug in the accountlines table creation

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -2273,6 +2273,7 @@ CREATE TABLE `accountlines` (
   `note` text NULL default NULL,
   `manager_id` int( 11 ) NULL,
   `meansofpayment` text NULL default NULL,
+  PRIMARY KEY (`id`),
   KEY `acctsborridx` (`borrowernumber`),
   KEY `timeidx` (`timestamp`),
   KEY `itemnumber` (`itemnumber`),
