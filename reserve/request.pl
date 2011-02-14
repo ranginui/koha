195ec93559277cd447374afe577a307c1ecfcd02
Paul Poulain <paul.poulain@biblibre.com>
2011-02-14 17:58:07 +0100

    MT5441: holdrestricted rules does not work properly
    The list of items that can have a hold place on them was not respecting the holdrestricted issuingrule.
    Now, an item can't have a hold placed if :
    * the item is not at the current librarian library
    * the holdrestricted is set
    * the Override syspref is not set

--- reserve/request.pl
+++ reserve/request.pl
@@ -405,8 +405,8 @@ foreach my $biblionumber (@biblionumbers) {
 
             my $branchitemrule = GetIssuingRule( $borrowerinfo->{categorycode}, $item->{'itype'}, GetReservesControlBranch( $borrowerinfo, $item ) );
             my $policy_holdrestricted = $branchitemrule->{'reservesallowed'};
-            $item->{'holdrestricted'} = $branchitemrule->{'holdrestricted'};
-
+            # prevent item to be reserved if holdrestricted & branch not the librarian branch & syspref to override not set
+            $item->{'holdrestricted'} = $branchitemrule->{'holdrestricted'} && (C4::Context->userenv->{branch} ne $item->{homebranch}) && !C4::Context->preference('AllowHoldPolicyOverride');
             if ( IsAvailableForItemLevelRequest($itemnumber) and not $item->{cantreserve} and CanItemBeReserved( $borrowerinfo->{borrowernumber}, $itemnumber ) ) {
                 if ( not $policy_holdrestricted and C4::Context->preference('AllowHoldPolicyOverride') ) {
                     $item->{override} = 1;
