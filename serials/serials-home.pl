62f4a8eb1126038da61f4db7cafa58320fab8b88
Matthias Meusburger <matthias.meusburger@biblibre.com>
2011-02-28 15:37:25 +0100

    MT5782: Adds an advanced per-site search for serials

--- serials/serials-home.pl
+++ serials/serials-home.pl
@@ -50,6 +50,7 @@ use C4::Context;
 my $query        = new CGI;
 my $title        = $query->param('title_filter');
 my $ISSN         = $query->param('ISSN_filter');
+my $branch       = $query->param('branch_filter');
 my $routing      = $query->param('routing') || C4::Context->preference("RoutingSerials");
 my $searched     = $query->param('searched');
 my $biblionumber = $query->param('biblionumber');
@@ -90,7 +91,7 @@ if (@serialseqs) {
 }
 my @subscriptions;
 if ($searched) {
-    @subscriptions = GetSubscriptions( $title, $ISSN, $biblionumber );
+    @subscriptions = GetSubscriptions( $title, $ISSN, $biblionumber, $branch );
 }
 
 # to toggle between create or edit routing list options
