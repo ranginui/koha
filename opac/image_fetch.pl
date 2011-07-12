cfc0e4bbfb3cbf29cf2966736386ccddfa07edab
Chris Cormack <chrisc@catalyst.net.nz>
2011-07-13 11:15:59 +1200

    Updating the lists and front page to use the new image fetcher

--- opac/image_fetch.pl
+++ opac/image_fetch.pl
@@ -2,6 +2,22 @@
 
 # Copyright Catalyst IT 2011
 
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+#
+
 
 use strict; 
 use LWP::Simple;
@@ -24,6 +40,9 @@ if ($type eq 'amazon'){
     }	
     elsif ($size eq 'shelfbrowse'){
 	$url =  "http://images.amazon.com/images/P/$id.01._AA75_PU_PU-5_.jpg";
+    }
+    elsif ($size eq 'large') {
+	$url =  "http://images.amazon.com/images/P/$id.01._THUMBZZZ_PB_PU_PU0_.jpg";
     }
 }
 
