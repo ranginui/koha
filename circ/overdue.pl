85b8730e64d2c061af908f25d49f1ecb81d49f9a
Julian Maurice <julian.maurice@biblibre.com>
2011-10-19 16:30:24 +0200

    Revert "MT3846: Adds item branch filter on overdues"
    This reverts commit 14bd1cdbecf835ac6219d18a36de5bc38508c050.

--- circ/overdue.pl
+++ circ/overdue.pl
@@ -37,7 +37,6 @@ my $borcatfilter   = $input->param('borcat') || '';
 my $itemtypefilter = $input->param('itemtype') || '';
 my $borflagsfilter = $input->param('borflag') || '';
 my $branchfilter   = $input->param('branch') || '';
-my $docbranchfilter= $input->param('docbranch') || '';
 my $op             = $input->param('op') || '';
 
 my $dateduefrom = format_date_in_iso($input->param( 'dateduefrom' )) || '';
@@ -87,8 +86,6 @@ my $onlymine =
   && C4::Context->userenv->{branch};
 
 $branchfilter = C4::Context->userenv->{'branch'} if ( $onlymine && !$branchfilter );
-
-my $homeorholdingbranch = C4::Context->preference('HomeOrHoldingBranch');
 
 # Filtering by Patron Attributes
 #  @patron_attr_filter_loop        is non empty if there are any patron attribute filters
@@ -281,7 +278,6 @@ if ($noreport) {
     $strsth .= " AND biblioitems.itemtype   = '" . $itemtypefilter . "' " if $itemtypefilter;
     $strsth .= " AND borrowers.flags        = '" . $borflagsfilter . "' " if $borflagsfilter;
     $strsth .= " AND borrowers.branchcode   = '" . $branchfilter . "' "   if $branchfilter;
-    $strsth .= " AND items.$homeorholdingbranch = '" . $docbranchfilter . "' " if $docbranchfilter; 
     $strsth .= " AND date_due < '" . $datedueto . "' "  if $datedueto;
     $strsth .= " AND date_due > '" . $dateduefrom . "' " if $dateduefrom;
   
